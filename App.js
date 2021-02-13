9fddc990cd86262318ddcb546a2cdc040a080967
Shrinivasan Neelamegam <shrinivasan.neelamegam@sap.com>
2021-02-13 12:05:00 +0530

    Init React Native App

--- App.js
+++ App.js
@@ -1,21 +1,53 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { TouchableHighlight, StyleSheet, Button, Text, View } from "react-native";
 
-export default function App() {
+
+const Cat = (props) => {
+  const [isHungry, setIsHungry] = useState(true);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+       <TouchableHighlight 
+            style ={{
+                    height: 40,
+                    width:350,
+                    borderRadius:10,
+                    backgroundColor : "black",
+                    marginLeft :50,
+                    marginRight:50,
+                    marginTop :20
+                }}>
+            <Button
+              color="orange"
+              onPress={() => {
+                setIsHungry(false);
+              }}
+              disabled={!isHungry}
+              title={isHungry ? "Click me, please! " : "Thanks for visiting V4 team!"}
+            />
+       </TouchableHighlight> 
+       <StatusBar style="auto" />
     </View>
   );
 }
+
+const Cafe = () => {
+  return (
+    <>
+      <Cat name="Munkustrap" />
+    </>
+  );
+}
+
+export default Cafe;
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
 });
